8114de36ad2aac9e30fedb4b5ddbb384206a184b
Abdoulie Njai <64009576+Phalconn@users.noreply.github.com>
2020-10-18 18:34:23 -0700

    Add files via upload
    Scenario:  You are an IT Support Administrator Specialist and is tasked with creating email accounts for new hires.

---
+++ emailApp.java
@@ -0,0 +1,11 @@
+package emailApp;
+
+public class emailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Abdoulie","Njai");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
